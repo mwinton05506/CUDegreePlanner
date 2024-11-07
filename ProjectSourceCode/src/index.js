50a55e45c435c5c41ce8c0f3e32c5b69e95b8535
Sallygator <nick.tishkowski@gmail.com>
2024-11-07 11:46:40 -0700

    update header, footer, scripting

---
+++ ProjectSourceCode/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// -------------------------------------  APP CONFIG   ----------------------------------------------
+
+// create `ExpressHandlebars` instance and configure the layouts and partials dir.
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+// set Session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
